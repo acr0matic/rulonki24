b3cde5388009b6cd6a2b580e6330e66bfacd63be
Nikita <main.acr0matic@gmail.com>
2020-08-27 01:44:52 +0300

    Сверстаны слайдеры в онлайн-примерочную

---
+++ src/scripts/script.js
@@ -0,0 +1,24 @@
+/* global Swiper */
+
+const swiperFull = new Swiper('.fitting__full', {
+  spaceBetween: 10,
+
+  loop: true,
+
+  slidesPerView: 1,
+  loopedSlides: 8,
+});
+
+const swiperThumbs = new Swiper('.fitting__thumbs', {
+  spaceBetween: 15,
+  slidesPerView: 8,
+  touchRatio: 0.2,
+  slideToClickedSlide: true,
+  loop: true,
+  loopedSlides: 8,
+  slideVisibleClass: 'fitting__swiper-slide',
+  slideActiveClass: 'fitting__swiper-slide--active',
+});
+
+swiperFull.controller.control = swiperThumbs;
+swiperThumbs.controller.control = swiperFull;
