c1739ef5cd32ec4e994a714d68cb84db6b117f25
Nikita <main.acr0matic@gmail.com>
2020-09-15 16:57:37 +0300

    Добавлена плавная навигация

---
+++ src/scripts/smoothscroll.js
@@ -0,0 +1,23 @@
+function SmoothScroll(blockID) {
+  const regex = /(\/?#?)/;
+  const id = blockID.replace(regex, '');
+
+  const destination = document.getElementById(id);
+
+  if (destination) {
+    destination.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  } else window.open(blockID, '_self');
+}
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+anchors.forEach((anchor) => {
+  anchor.addEventListener('click', (e) => {
+    e.preventDefault();
+    const blockID = anchor.getAttribute('href');
+
+    SmoothScroll(blockID);
+  });
+});
